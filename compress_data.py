80381a51006b6020de30d39b23b1384e2388323c
juhuntenburg <ju.huntenburg@gmail.com>
2019-07-22 09:46:36 +0100

    new pc setup

--- compress_data.py
+++ compress_data.py
@@ -3,14 +3,14 @@ from glob import glob
 import numpy as np
 import os
 
-mask = "/home/julia/projects/gradients/data_jo/atlas/isocortex.nii.gz"
+mask = "/home/julia/projects/gradients/data_jo/atlas/cortex_mask.nii.gz"
 func = glob("/home/julia/projects/gradients/data_jo/orig/*MEDISO*.nii.gz")
 
 masker = NiftiMasker(mask_img=mask, standardize=True)
 
 for f in func:
     func_compressed = masker.fit_transform(f)
-    np.save('/home/julia/projects/gradients/data_jo/compressed/%s.npy'
+    np.save('/home/julia/projects/gradients/data_jo/compressed/in/%s.npy'
             % os.path.basename(f).split(".")[0], func_compressed)
 
     print(f)
