0874ee6b2e2bc584c2e2eac84cbd0c46f3ea8e6d
juhuntenburg <ju.huntenburg@gmail.com>
2019-08-28 17:28:37 +0100

    finishing layer masks

--- compress_data.py
+++ compress_data.py
@@ -3,14 +3,14 @@ from glob import glob
 import numpy as np
 import os
 
-mask = "/home/julia/projects/gradients/data_jo/atlas/cortex_mask.nii.gz"
-func = glob("/home/julia/projects/gradients/data_jo/orig/*MEDISO*.nii.gz")
+mask = "/home/julia/data/gradients/atlas/cortex/cortex_mask_25um.nii.gz"
+func = glob("/home/julia/data/gradients/orig/v2/*MEDISO*EPI*.nii.gz")
 
 masker = NiftiMasker(mask_img=mask, standardize=True)
 
 for f in func:
     func_compressed = masker.fit_transform(f)
-    np.save('/home/julia/projects/gradients/data_jo/compressed/in/%s.npy'
+    np.save('/home/julia/data/gradients/compressed/v2/%s.npy'
             % os.path.basename(f).split(".")[0], func_compressed)
 
     print(f)
