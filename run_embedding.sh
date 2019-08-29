948c109e687136d833a0b24bae0cd8a6ee0545e3
juhuntenburg <ju.huntenburg@gmail.com>
2019-08-29 12:02:19 +0100

    adapting cluster run files

--- run_embedding.sh
+++ run_embedding.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Install required libraries on top of existing docker image
-pip install numexpr h5py numpy scipy sklearn nilearn nibabel
-apt-get -y install git
-pip install git+https://github.com/juhuntenburg/hcp_corr.git@enh/python3
-pip install git+https://github.com/satra/mapalign.git
+pip install --user numexpr h5py numpy scipy sklearn nilearn nibabel
+pip install --user git+https://github.com/juhuntenburg/hcp_corr.git@enh/python3
+pip install --user git+https://github.com/satra/mapalign.git
 
 # run compression script
 python compress_data.py
