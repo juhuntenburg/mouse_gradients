f93cf6ae21850b3a5ce08cf8f5df7d8a3511316e
juhuntenburg <ju.huntenburg@gmail.com>
2019-08-30 10:26:09 +0100

    testing condor

--- run_embedding.sh
+++ run_embedding.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # set two environment variables for git
-export GIT_COMMITTER_NAME=juhuntenburg
-export GIT_COMMITTER_EMAIL=ju.huntenburg@gmail.com
+# export GIT_COMMITTER_NAME=juhuntenburg
+# export GIT_COMMITTER_EMAIL=ju.huntenburg@gmail.com
 
 # Install required libraries on top of existing docker image
 pip install --user numexpr h5py numpy scipy sklearn nilearn nibabel
@@ -10,7 +10,10 @@ pip install --user git+https://github.com/juhuntenburg/hcp_corr.git@enh/python3
 pip install --user git+https://github.com/satra/mapalign.git
 
 # run compression script
-python compress_data.py
+echo "Start compression"
+python $@
+echo "Done compression"
 
 # run embedding script
-python embedding_compressed.py
+# python embedding_compressed.py
+# echo "Done embedding"
