c951edabe79effdb684c25476332368578469305
juhuntenburg <ju.huntenburg@gmail.com>
2019-09-04 11:11:47 +0100

    fixed output on cluster

--- compress_data.py
+++ compress_data.py
@@ -3,14 +3,14 @@ from glob import glob
 import numpy as np
 import os
 
-mask = "/nfs/tank/embedding/cortex_mask_25um.nii.gz"
-func = glob("/nfs/tank/embedding/orig/*MEDISO*EPI*.nii.gz")
+mask = "/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/embedding/cortex_mask_25um.nii.gz"
+func = glob("/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/embedding/orig/*MEDISO*EPI*.nii.gz")
 
 masker = NiftiMasker(mask_img=mask, standardize=True)
 
 for f in func:
     func_compressed = masker.fit_transform(f)
-    np.save('embedding/compressed/%s.npy'
+    np.save('/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/embedding/compressed/%s.npy'
             % os.path.basename(f).split(".")[0], func_compressed)
 
     print(f)
