779e4c4430bb5f84a3539672f3ee11203e4e66a8
juhuntenburg <ju.huntenburg@gmail.com>
2019-09-25 11:03:36 +0100

    compress with smoothing

--- compress_data.py
+++ compress_data.py
@@ -6,11 +6,11 @@ import os
 mask = "/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/embedding/cortex_mask_25um.nii.gz"
 func = glob("/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/embedding/orig/*MEDISO*EPI*.nii.gz")
 
-masker = NiftiMasker(mask_img=mask, standardize=True)
+masker = NiftiMasker(mask_img=mask, standardize=True, smoothing_fwhm=0.45)
 
 for f in func:
     func_compressed = masker.fit_transform(f)
-    np.save('/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/embedding/compressed/%s.npy'
+    np.save('/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/embedding/compressed_045/%s.npy'
             % os.path.basename(f).split(".")[0], func_compressed)
 
     print(f)
