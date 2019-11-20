1c1121939e7fda518c0caeb1fcbaede6629df208
juhuntenburg <ju.huntenburg@gmail.com>
2019-11-20 11:59:16 +0000

    finish mesh sampling and clean up

--- mesh_sampling.py
+++ mesh_sampling.py
@@ -82,7 +82,21 @@ def profile_sampling(data, profiles):
 mesh = io.load_mesh_geometry("/home/julia/data/gradients/atlas/allen_api/brain_mesh.vtk")
 normals = np.load("/home/julia/data/gradients/atlas/allen_api/brain_mesh_normals.npy")
 mask = nb.load("/home/julia/data/gradients/atlas/allen_api/cortex_mask_tight_200um.nii.gz").get_data()
-lvl = nb.load("/home/julia/data/gradients/atlas/allen_api/cortex_mask_tight_lvl_200um.nii.gz").get_data()
 
 profiles = find_profiles(mesh, normals, mask, resolution=200)
+with open('/home/julia/data/gradients/results/mesh_sampling/profiles.pkl', 'wb') as f:
+    pickle.dump(profiles, f)
+
+# For QA
+lvl = nb.load("/home/julia/data/gradients/atlas/allen_api/cortex_mask_tight_lvl_200um.nii.gz").get_data()
 mesh_data = profile_sampling(lvl, profiles)
+mesh_data = np.nan_to_num(mesh_data)
+mesh['data']=mesh_data
+io.save_mesh('/home/julia/data/gradients/results/mesh_sampling/lvl_qa_mesh_sampling.vtk', mesh)
+
+profile_length = []
+for p in range(len(mesh["points"])):
+    profile_length.append(len(profiles[p]))
+profile_length=np.asarray(profile_length)
+mesh['data'] = profile_length
+io.save_mesh('/home/julia/data/gradients/results/mesh_sampling/profile_length_mesh_sampling.vtk', mesh)
