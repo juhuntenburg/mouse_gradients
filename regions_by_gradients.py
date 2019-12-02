5f82c8d5d324220533ef47f257c146f72e212d69
juhuntenburg <ju.huntenburg@gmail.com>
2019-12-02 12:10:47 +0000

    cytoarchitecture analysis, clean up

--- regions_by_gradients.py
+++ regions_by_gradients.py
@@ -4,11 +4,11 @@ import pandas as pd
 import pickle
 
 # Load images and structure tree
-regions = np.array(nb.load('/home/julia/data/gradients/atlas/allen_api/regions/isocortex_annot_finer_200um.nii.gz').get_data(), dtype='float64')
+annot = np.array(nb.load('/home/julia/data/gradients/atlas/allen_api/regions/annot_finest_200um.nii.gz').get_data(), dtype='float64')
 gradient = nb.load('/home/julia/data/gradients/results/embedding_vol/embed.nii.gz').get_data()
 mask = nb.load('/home/julia/data/gradients/atlas/allen_api/cortex_mask_tight_200um.nii.gz').get_data()
-with open('/home/julia/data/gradients/atlas/allen_api/regions/isocortex_annot_finer.pkl', 'rb') as f:
-    regions_dict = pickle.load(f)
+df = pd.read_csv('/home/julia/data/gradients/results/regions/finest_regions.csv')
+df = df.drop(columns=["Unnamed: 0"])
 
 # Caculate first the region means for each gradient
 for g in range(6):
@@ -17,13 +17,9 @@ for g in range(6):
     g_masked[mask==0] = np.nan
 
     region_means = []
-    for region in regions_dict.values():
-        region_means.append(np.nanmean(g_masked[regions==region]))
-    region_means = np.array(region_means)
+    for region in df['id']:
+        region_means.append(np.nanmean(g_masked[annot==region]))
 
-    df = pd.DataFrame(data=zip(regions_dict.keys(), regions_dict.values(), region_means),
-                      columns=['region', 'idx','mean'])
+    df['gradient %i' %g] = region_means
 
-    df = df.sort_values(by='mean')
-
-    df.to_csv('/home/julia/data/gradients/results/regions/finer_regions_by_gradients_%s.csv' %str(g))
+df.to_csv('/home/julia/data/gradients/results/regions/finest_regions.csv')
