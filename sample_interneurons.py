1628d6a4630017cdf9b9c952b1f4b1e52895c88c
juhuntenburg <ju.huntenburg@gmail.com>
2020-02-19 13:44:37 +0000

    some interneuron updates

---
+++ sample_interneurons.py
@@ -0,0 +1,23 @@
+import os
+import pickle
+from glob import glob
+import numpy as np
+import nibabel as nb
+from nighres import io
+from functions import profile_sampling
+
+
+# Load previously created profiles
+with open('/home/julia/data/gradients/results/profiles/profiles.pkl', 'rb') as pf:
+    profiles=pickle.load(pf)
+
+data = nb.load('/home/julia/data/gradients/atlas/interneurons/vip2allen.nii.gz').get_data()
+mask = nb.load('/home/julia/data/gradients/atlas/allen_api/cortex_mask_tight_200um.nii.gz').get_data()
+data[:,:,:][mask==0] = np.nan
+
+data_mesh = np.squeeze(profile_sampling(data[:,:,:], profiles))
+
+# load and save data on mesh
+mesh = io.load_mesh('/home/julia/data/gradients/atlas/allen_api/regions/annot_finest.vtk')
+mesh['data'] = np.nan_to_num(data_mesh)
+io.save_mesh('/home/julia/data/gradients/results/interneurons/vip_all_sampled_mesh.vtk', mesh)
