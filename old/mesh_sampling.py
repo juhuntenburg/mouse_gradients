fbd01d6ed48cc31495f5a32031b0508a25f754c0
juhuntenburg <ju.huntenburg@gmail.com>
2020-03-01 12:12:56 +0000

    cleaning up unused scripts

--- old/mesh_sampling.py
+++
@@ -1,3 +0,0 @@
-import nighres
-
-meshing=nighres.surface.surface_mesh_mapping("/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/layers/vol0005.nii.gz", "/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/layers/brain_mesh.vtk", mapping_method="closest_point", save_data=True, overwrite=True, output_dir='/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/layers/')
--- old/profile_sampling.py
+++
@@ -1,3 +0,0 @@
-import nighres
-
-profiles=nighres.laminar.profile_sampling("/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/layers/inner_p2l-surf_layering-boundaries.nii.gz", "/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/layers/annotation.nii.gz", save_data=True, overwrite=True, output_dir='/nfs/tank/shemesh/users/julia.huntenburg/rodent_gradients/layers/')
