a489e8801d5db3f3878aa02e95f3fd71c5439f17
julia <juhuntenburg>
2020-06-08 16:52:56 +0100

    starting to rerun and clean up for revisions

--- 03_Mesh_Sampling/find_profiles.py
+++ 03_Mesh_Sampling/find_profiles.py
@@ -4,16 +4,18 @@ import nibabel as nb
 import pickle
 from glob import glob
 import os
-from functions import find_profiles, profile_sampling
+from profile_functions import find_profiles, profile_sampling
 
 
-mesh = io.load_mesh_geometry("/home/julia/data/gradients/atlas/allen_api/brain_mesh.vtk")
-normals = np.load("/home/julia/data/gradients/atlas/allen_api/brain_mesh_normals.npy")
-mask = nb.load("/home/julia/data/gradients/atlas/allen_api/cortex_mask_tight_200um.nii.gz").get_data()
+mesh = io.load_mesh_geometry("/home/julia/data/gradients/allen_atlas/brain_mesh.vtk")
+normals = np.load("/home/julia/data/gradients/allen_atlas/brain_mesh_normals.npy")
+# Brain mesh normals created by loading mesh into paraview, using filter "GenerateSurfaceNormals",
+# saving the result as csv and then converting the normals to npy
+mask = nb.load("/home/julia/data/gradients/allen_atlas/cortex_mask_tight_200um.nii.gz").get_data()
 
 # Create the profiles
 profiles = find_profiles(mesh, normals, mask, resolution=200)
-with open('/home/julia/data/gradients/results/mesh_sampling/profiles.pkl', 'wb') as f:
+with open('/home/julia/data/gradients/results/profiles/profiles.pkl', 'wb') as f:
     pickle.dump(profiles, f)
 
 # Calculate profile length for QA
@@ -25,7 +27,7 @@ mesh['data'] = profile_length
 io.save_mesh('/home/julia/data/gradients/results/profiles/profile_length_mesh_sampling.vtk', mesh)
 
 # Sample levelset for QA
-lvl = nb.load("/home/julia/data/gradients/atlas/allen_api/cortex_mask_tight_lvl_200um.nii.gz").get_data()
+lvl = nb.load("/home/julia/data/gradients/allen_atlas/cortex_mask_tight_lvl_200um.nii.gz").get_data()
 mesh_data = profile_sampling(lvl, profiles)
 mesh_data = np.nan_to_num(mesh_data)
 mesh['data']=mesh_data
