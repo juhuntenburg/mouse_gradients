1a9fe4f459ace668b57b31bcdb1633963ebe3153
julia <juhuntenburg>
2020-06-25 16:02:23 +0100

    visualization of repro gradients

--- 03_Mesh_Sampling/sample_gradients_viz.py
+++ 03_Mesh_Sampling/sample_gradients_viz.py
@@ -4,18 +4,18 @@ from glob import glob
 import numpy as np
 import nibabel as nb
 from nighres import io
-from functions import profile_sampling
+from profile_functions import profile_sampling
 
 
 # Load previously created profiles
 with open('/home/julia/data/gradients/results/profiles/profiles.pkl', 'rb') as pf:
     profiles=pickle.load(pf)
-mesh = io.load_mesh('/home/julia/data/gradients/atlas/allen_api/annotation/annot.vtk')
+mesh = io.load_mesh('/home/julia/data/gradients/allen_atlas/brain_mesh.vtk')
 
 
 for d in ['ad2', 'ad3', 'csd']:
     # Load gradient image
-    data = nb.load('/home/julia/data/gradients/results/repro/%s_embed.nii.gz' %d).get_data()
+    data = nb.load('/home/julia/data/gradients/results/repro/%s/%s_embed.nii.gz' %(d,d)).get_data()
     data_mesh = np.zeros((len(profiles),))
 
     for t in range(6):
@@ -24,9 +24,9 @@ for d in ['ad2', 'ad3', 'csd']:
         data_mesh_min[np.isnan(data_mesh)] = np.nanmin(data_mesh)
         data_mesh_max = data_mesh.copy()
         data_mesh_max[np.isnan(data_mesh)] = np.nanmax(data_mesh)
-        io.save_mesh('/home/julia/data/gradients/results/repro/%s_embed_viz_g%i_min.vtk'%(d,t),
+        io.save_mesh('/home/julia/data/gradients/results/repro/%s/%s_embed_viz_g%i_min.vtk'%(d,d,t),
                      {'faces':mesh['faces'], 'points':mesh['points'],
                        'data':data_mesh_min})
-        io.save_mesh('/home/julia/data/gradients/results/repro/%s_embed_viz_g%i_max.vtk'%(d,t),
+        io.save_mesh('/home/julia/data/gradients/results/repro/%s/%s_embed_viz_g%i_max.vtk'%(d,d,t),
                      {'faces':mesh['faces'], 'points':mesh['points'],
                        'data':data_mesh_max})
--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM ubuntu:18.04
-
-RUN apt-get update
-RUN apt-get install -y python3 python3-pip
-
-RUN python3 -m pip install numpy scipy cython
-RUN python3 -m pip install gdist
