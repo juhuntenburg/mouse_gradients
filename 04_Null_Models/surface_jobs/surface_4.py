8484c3a32c8d719ca6090d591f15cd83a85b3fda
julia <juhuntenburg>
2020-07-07 15:58:15 +0100

    joblib parallelization of surrogates

--- 04_Null_Models/surface_jobs/surface_4.py
+++ 04_Null_Models/surface_jobs/surface_4.py
@@ -1,46 +1,55 @@
+import pickle
 import numpy as np
+from joblib import Parallel, delayed
 from brainsmash.mapgen.sampled import Sampled
 
-data_dir = '/nfs/tank/shemesh/users/julia.huntenburg/revisions/null_models/surface/'
-gradients = np.load(data_dir+'gradients1_6.npy')
-distmat = data_dir+'distmat.npy'
-index = data_dir+'index.npy'
-surr_file = data_dir+'surf4_surrogates.npy'
-emp_var_file = data_dir+'surf4_emp_var.npy'
-surr_var_file = data_dir+'surf4_surr_var.npy'
-u0_file = data_dir+'surf4_u0.npy'
 
-nsurr = 100000
+def compute_surrogates(n_gradient, n_perms):
 
-# Construct sampled class
-sampled = Sampled(gradients[:,4], distmat, index,
+    data_dir = '/nfs/tank/shemesh/users/julia.huntenburg/revisions/null_models/surface/'
+    gradients = np.load(data_dir+'gradients1_6.npy')
+    distmat = data_dir+'distmat.npy'
+    index = data_dir+'index.npy'
+    dict_file = data_dir+'surf{}_n{}.pkl'
+
+    nsurr = 1000
+    surr_dict ={}
+
+    # Construct sampled class
+    sampled = Sampled(gradients[:,n_gradient], distmat, index,
                      deltas=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9],
                      knn=3000,
                      ns=1000)
 
+    # Compute and save surrogates
+    surrogates = sampled(n=nsurr)
+    surr_dict['maps'] = surrogates
 
-# Compute and save surrogates
-surrogates = sampled(n=nsurr)
-np.save(surr_file, surrogates)
+    # Compute and saving variograms
+    surr_var = np.empty((nsurr, sampled.nh))
+    emp_var_samples = np.empty((nsurr, sampled.nh))
+    u0_samples = np.empty((nsurr, sampled.nh))
+    for i in range(nsurr):
+        idx = sampled.sample()  # Randomly sample a subset of brain areas
+        v = sampled.compute_variogram(sampled.x, idx)
+        u = sampled.D[idx, :]
+        umax = np.percentile(u, sampled.pv)
+        uidx = np.where(u < umax)
+        emp_var_i, u0i = sampled.smooth_variogram(
+            u=u[uidx], v=v[uidx], return_h=True)
+        emp_var_samples[i], u0_samples[i] = emp_var_i, u0i
+        # Surrogate
+        v_null = sampled.compute_variogram(surrogates[i], idx)
+        surr_var[i] = sampled.smooth_variogram(
+            u=u[uidx], v=v_null[uidx], return_h=False)
 
-# Compute and saving variograms
-surr_var = np.empty((nsurr, sampled.nh))
-emp_var_samples = np.empty((nsurr, sampled.nh))
-u0_samples = np.empty((nsurr, sampled.nh))
-for i in range(nsurr):
-    idx = sampled.sample()  # Randomly sample a subset of brain areas
-    v = sampled.compute_variogram(sampled.x, idx)
-    u = sampled.D[idx, :]
-    umax = np.percentile(u, sampled.pv)
-    uidx = np.where(u < umax)
-    emp_var_i, u0i = sampled.smooth_variogram(
-        u=u[uidx], v=v[uidx], return_h=True)
-    emp_var_samples[i], u0_samples[i] = emp_var_i, u0i
-    # Surrogate
-    v_null = sampled.compute_variogram(surrogates[i], idx)
-    surr_var[i] = sampled.smooth_variogram(
-        u=u[uidx], v=v_null[uidx], return_h=False)
+    surr_dict['emp_var']=emp_var_samples
+    surr_dict['u0']=u0_samples
+    surr_dict['surr_var']=surr_var
 
-np.save(emp_var_file, emp_var_samples)
-np.save(u0_file, u0_samples)
-np.save(surr_var_file, surr_var)
+    pkl_out = open(dict_file.format(n_gradient, n_perms), 'wb')
+    pickle.dump(surr_dict, pkl_out)
+    pkl_out.close()
+
+
+Parallel(n_jobs=10)(delayed(compute_surrogates)(4,i) for i in range(10))
